a0b448e5a6d0413544eed9d240804bcbf851af2a
Nathan Duckett <nduckett13@gmail.com>
2018-10-11 13:30:28 +1300

    horizontal line method

--- RobotArm.java
+++ RobotArm.java
@@ -27,8 +27,25 @@ public class RobotArm {
 
     }
 
+    private void generateHorizontal() {
+        try {
+            output = new PrintStream(new File("horizontal.txt"));
+
+            output.println("1500,1500,1600");
+            output.println("1300,1300,1600");
+            for (int i = 1300; i < 1700; i += 50) {
+                output.printf("%d,%d,1400\n", i, i);
+            }
+            output.println("1700,1700,1600");
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error loading file");
+        }
+    }
+
     public static void main(String args[]) {
         RobotArm r = new RobotArm();
         r.convertToSignals();
+        r.generateHorizontal();
     }
 }
